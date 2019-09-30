161219e0f2eade4edbe76bd1f0d7295f08b0b6af
oscar2706 <spat_2706@outlook.com>
2019-09-29 21:29:33 -0500

    Agregado Componente vistaPrincipal

--- src/app/tutor/tutor.module.ts
+++ src/app/tutor/tutor.module.ts
@@ -1,10 +1,14 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { VistaPrincipalComponent } from './vista-principal/vista-principal.component';
 
 @NgModule({
-  declarations: [],
+  declarations: [VistaPrincipalComponent],
   imports: [
     CommonModule
+  ],
+  exports: [
+    VistaPrincipalComponent
   ]
 })
 export class TutorModule { }

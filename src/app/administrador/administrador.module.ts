7dea752f2a81d5ca7f9d4501eb07a1939df96d72
FannySarb <50083314+FannySarb@users.noreply.github.com>
2019-09-30 21:42:28 -0500

    componente central en Administrador

--- src/app/administrador/administrador.module.ts
+++ src/app/administrador/administrador.module.ts
@@ -2,9 +2,10 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { BarraComponent } from './barra/barra.component';
 import { ColumnaIzqComponent } from './columna-izq/columna-izq.component';
+import { CentralComponent } from './central/central.component';
 
 @NgModule({
-  declarations: [BarraComponent, ColumnaIzqComponent],
+  declarations: [BarraComponent, ColumnaIzqComponent, CentralComponent],
   imports: [
     CommonModule
   ]

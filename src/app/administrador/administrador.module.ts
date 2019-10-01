55236a4690b96b5118bf3366a00c02188c1a3640
FannySarb <50083314+FannySarb@users.noreply.github.com>
2019-09-30 22:48:52 -0500

    Revert "componente columna izquierda en Administrador"
    This reverts commit 42005e018c765b9b77aab0992291459c95b4bfa5.

--- src/app/administrador/administrador.module.ts
+++ src/app/administrador/administrador.module.ts
@@ -1,10 +1,9 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { BarraComponent } from './barra/barra.component';
-import { ColumnaIzqComponent } from './columna-izq/columna-izq.component';
 
 @NgModule({
-  declarations: [BarraComponent, ColumnaIzqComponent],
+  declarations: [BarraComponent],
   imports: [
     CommonModule
   ]

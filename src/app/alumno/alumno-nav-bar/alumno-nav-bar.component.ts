bdde0988d5fae6da14f08c2e2c787266eb1bfe6b
LuisLeonardo98 <luis.leonardo.98@hotmail.com>
2019-09-30 22:43:30 -0500

    NavBar Alumno

---
+++ src/app/alumno/alumno-nav-bar/alumno-nav-bar.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-alumno-nav-bar',
+  templateUrl: './alumno-nav-bar.component.html',
+  styleUrls: ['./alumno-nav-bar.component.css']
+})
+export class AlumnoNavBarComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
--- src/app/alumno/alumno.module.ts
+++ src/app/alumno/alumno.module.ts
@@ -1,10 +1,12 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { AlumnoNavBarComponent } from './alumno-nav-bar/alumno-nav-bar.component';
 
 @NgModule({
-  declarations: [],
+  declarations: [AlumnoNavBarComponent],
   imports: [
     CommonModule
-  ]
+  ],
+  exports: [AlumnoNavBarComponent]
 })
 export class AlumnoModule { }

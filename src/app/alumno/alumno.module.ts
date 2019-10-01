f905f6c4fcb0d670bcd098f61142a086e5f9da7a
LuisLeonardo98 <luis.leonardo.98@hotmail.com>
2019-10-01 12:07:27 -0500

    Tabla Proyecciones Alumno
    Agregado Tabla Angular y Archivos en Main

--- src/app/alumno/alumno.module.ts
+++ src/app/alumno/alumno.module.ts
@@ -1,13 +1,17 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+
 import { AlumnoNavBarComponent } from './alumno-nav-bar/alumno-nav-bar.component';
 import { AlumnoPanelComponent } from './alumno-panel/alumno-panel.component';
 import { AlumnoProyeccionComponent } from './alumno-proyeccion/alumno-proyeccion.component';
 
+
 @NgModule({
   declarations: [AlumnoNavBarComponent, AlumnoPanelComponent, AlumnoProyeccionComponent],
   imports: [
-    CommonModule
+    CommonModule, NgbModule, FormsModule, ReactiveFormsModule
   ],
   exports: [AlumnoNavBarComponent, AlumnoPanelComponent, AlumnoProyeccionComponent]
 })

5cdf03874393bb611e33935fbcacd9f7f884193b
FannySarb <50083314+FannySarb@users.noreply.github.com>
2019-09-30 22:49:03 -0500

    Revert "Agregar componente barra a Administrador"
    This reverts commit 455c2371ddcabfcc4fac4ffa95bbeddbfbf2497f.

--- src/app/administrador/administrador.module.ts
+++ src/app/administrador/administrador.module.ts
@@ -1,9 +1,8 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { BarraComponent } from './barra/barra.component';
 
 @NgModule({
-  declarations: [BarraComponent],
+  declarations: [],
   imports: [
     CommonModule
   ]

455c2371ddcabfcc4fac4ffa95bbeddbfbf2497f
FannySarb <50083314+FannySarb@users.noreply.github.com>
2019-09-30 21:19:40 -0500

    Agregar componente barra a Administrador

--- src/app/administrador/administrador.module.ts
+++ src/app/administrador/administrador.module.ts
@@ -1,8 +1,9 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { BarraComponent } from './barra/barra.component';
 
 @NgModule({
-  declarations: [],
+  declarations: [BarraComponent],
   imports: [
     CommonModule
   ]

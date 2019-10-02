cb20680d3e64333b26dfc3445848e3817fb372e8
FannySarb <50083314+FannySarb@users.noreply.github.com>
2019-10-02 17:16:40 -0500

    creación de Interfaz restablecer contraseña

--- src/app/administrador/administrador.module.ts
+++ src/app/administrador/administrador.module.ts
@@ -1,17 +1,18 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 import { NavbarComponent } from './navbar/navbar.component';
 import { BarLeftComponent } from './bar-left/bar-left.component';
 import { CentralComponent } from './central/central.component';
 import { Central2Component } from './central2/central2.component';
 import { ResetPasswdComponent } from './reset-passwd/reset-passwd.component';
 import { ProyeccionesComponent } from './proyecciones/proyecciones.component';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 @NgModule({
-  declarations: [NavbarComponent, BarLeftComponent, CentralComponent, Central2Component, ResetPasswdComponent, ProyeccionesComponent,
-    Central2Component,ResetPasswdComponent,ProyeccionesComponent],
+  declarations: [NavbarComponent, BarLeftComponent, CentralComponent,Central2Component,ResetPasswdComponent,ProyeccionesComponent],
   imports: [
-    CommonModule
+    CommonModule, NgbModule,FormsModule, ReactiveFormsModule
   ],
   exports:[NavbarComponent,BarLeftComponent,CentralComponent,Central2Component,ResetPasswdComponent,ProyeccionesComponent]
 })
--- src/app/administrador/central/central.component.ts
+++ src/app/administrador/central/central.component.ts
@@ -1,15 +1,82 @@
 import { Component, OnInit } from '@angular/core';
+interface Country {
+  name: string;
+  area: number;
 
+}
+
+const COUNTRIES: Country[] = [
+  {
+    name: 'Sarmiento Barrios Estefania',
+    area: 17075200
+  },
+  {
+    name: 'Montiel Estrada Mayra vanessa ',
+    area: 640679
+  },
+  {
+    name: 'Amador Barrientos Luis Leonardo',
+    area: 357114
+  },
+  {
+    name: 'Rubio Quintero Rebeca',
+    area: 92090
+  },
+  {
+    name: 'Patricio Hernández Oscar',
+    area: 9976140
+  },
+  {
+    name: 'Lozada Mendoza Dylan Salomón',
+    area: 331212
+  },
+  {
+    name: 'Hernández Cuacua Rosa Gabriela',
+    area: 8515767
+  },
+  {
+    name: 'Sandra Danae Rosas Ortega',
+    area: 1964375
+  },
+  {
+    name: 'Citlali Castillo Morales',
+    area: 9629091
+  },
+  {
+    name: 'José Luis Morales González',
+    area: 3287263
+  },
+  {
+    name: 'Victoria Rubio Quintero',
+    area: 1910931
+  },
+  {
+    name: 'Ana Paula Sarmiento Barrios',
+    area: 26
+  },
+  {
+    name: 'Lizbeth Gaspar Antonio',
+    area: 9596960
+  }
+];
 @Component({
   selector: 'app-central',
   templateUrl: './central.component.html',
   styleUrls: ['./central.component.css']
 })
 export class CentralComponent implements OnInit {
-
+  
   constructor() { }
 
-  ngOnInit() {
+  ngOnInit() {}
+  page = 1;
+  pageSize = 4;
+  collectionSize = COUNTRIES.length;
+
+  get countries(): Country[] {
+    return COUNTRIES
+      .map((country, i) => ({id: i + 1, ...country}))
+      .slice((this.page - 1) * this.pageSize, (this.page - 1) * this.pageSize + this.pageSize);
   }
 
 }

50e06d4b712007d519b48a17f0678d523810ef0a
LuisLeonardo98 <luis.leonardo.98@hotmail.com>
2019-10-01 22:45:42 -0500

    Ventana de Proyeccion del Alumno Version 1

--- src/app/alumno/alumno.module.ts
+++ src/app/alumno/alumno.module.ts
@@ -1,24 +1,31 @@
-import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
-import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
-import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { NgModule } from "@angular/core";
+import { CommonModule } from "@angular/common";
+import { NgbModule } from "@ng-bootstrap/ng-bootstrap";
+import { FormsModule, ReactiveFormsModule } from "@angular/forms";
 
-import { AlumnoNavBarComponent } from './alumno-nav-bar/alumno-nav-bar.component';
-import { AlumnoPanelComponent } from './alumno-panel/alumno-panel.component';
-import { AlumnoProyeccionComponent } from './alumno-proyeccion/alumno-proyeccion.component';
-import { AlumnoMateriasComponent } from './alumno-materias/alumno-materias.component';
-import { AlumnoMateriaComponent } from './alumno-materia/alumno-materia.component';
-import { AlumnoOptativasComponent } from './alumno-optativas/alumno-optativas.component';
-import { AlumnoVentanaMateriasComponent } from './alumno-ventana-materias/alumno-ventana-materias.component';
-import { AlumnoVentanaOptativasComponent } from './alumno-ventana-optativas/alumno-ventana-optativas.component';
-import { AlumnoVentanaProyeccionComponent } from './alumno-ventana-proyeccion/alumno-ventana-proyeccion.component';
-
+import { AlumnoNavBarComponent } from "./alumno-nav-bar/alumno-nav-bar.component";
+import { AlumnoPanelComponent } from "./alumno-panel/alumno-panel.component";
+import { AlumnoProyeccionComponent } from "./alumno-proyeccion/alumno-proyeccion.component";
+import { AlumnoMateriasComponent } from "./alumno-materias/alumno-materias.component";
+import { AlumnoMateriaComponent } from "./alumno-materia/alumno-materia.component";
+import { AlumnoOptativasComponent } from "./alumno-optativas/alumno-optativas.component";
+import { AlumnoVentanaMateriasComponent } from "./alumno-ventana-materias/alumno-ventana-materias.component";
+import { AlumnoVentanaOptativasComponent } from "./alumno-ventana-optativas/alumno-ventana-optativas.component";
+import { AlumnoVentanaProyeccionComponent } from "./alumno-ventana-proyeccion/alumno-ventana-proyeccion.component";
 
 @NgModule({
-  declarations: [AlumnoNavBarComponent, AlumnoPanelComponent, AlumnoProyeccionComponent, AlumnoMateriasComponent, AlumnoMateriaComponent, AlumnoOptativasComponent, AlumnoVentanaMateriasComponent, AlumnoVentanaOptativasComponent, AlumnoVentanaProyeccionComponent],
-  imports: [
-    CommonModule, NgbModule, FormsModule, ReactiveFormsModule
+  declarations: [
+    AlumnoNavBarComponent,
+    AlumnoPanelComponent,
+    AlumnoProyeccionComponent,
+    AlumnoMateriasComponent,
+    AlumnoMateriaComponent,
+    AlumnoOptativasComponent,
+    AlumnoVentanaMateriasComponent,
+    AlumnoVentanaOptativasComponent,
+    AlumnoVentanaProyeccionComponent
   ],
-  exports: [AlumnoNavBarComponent, AlumnoPanelComponent, AlumnoProyeccionComponent, AlumnoMateriaComponent]
+  imports: [CommonModule, NgbModule, FormsModule, ReactiveFormsModule],
+  exports: [AlumnoVentanaProyeccionComponent]
 })
-export class AlumnoModule { }
+export class AlumnoModule {}

42a3b9a6f1bb51ea0b6eaded53da3ab44ebeff28
FannySarb <50083314+FannySarb@users.noreply.github.com>
2019-10-06 21:07:41 -0500

    correccion modal

--- src/app/administrador/central/central.component.ts
+++ src/app/administrador/central/central.component.ts
@@ -5,16 +5,16 @@ import { NgbActiveModal, NgbModal } from '@ng-bootstrap/ng-bootstrap';
   selector: 'ngbd-modal-content',
   template: `
     <div class="modal-header">
-      <h4 class="modal-title">Hi there!</h4>
+      <h4 class="modal-title">Exito</h4>
       <button type="button" class="close" aria-label="Close" (click)="activeModal.dismiss('Cross click')">
         <span aria-hidden="true">&times;</span>
       </button>
     </div>
     <div class="modal-body">
-      <p>Hello, {{name}}!</p>
+      <p>La contraseña se restauro correctamente</p>
     </div>
     <div class="modal-footer">
-      <button type="button" class="btn btn-outline-dark" (click)="activeModal.close('Close click')">Close</button>
+      <button type="button" class="btn btn-outline-dark" (click)="activeModal.close('Close click')">Ok</button>
     </div>
   `
 })

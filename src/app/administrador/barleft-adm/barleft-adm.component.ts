66f5b8ea1eff52558c92abd3b35b073c122ae244
FannySarb <50083314+FannySarb@users.noreply.github.com>
2019-10-08 00:01:17 -0500

    modificaciones en datepicker

--- src/app/administrador/barleft-adm/barleft-adm.component.ts
+++ src/app/administrador/barleft-adm/barleft-adm.component.ts
@@ -1,13 +1,18 @@
 import { Component, OnInit } from '@angular/core';
+import   {   NgbDateCustomParserFormatter }   from   '../../administrador/dateformat' ; 
+import   {   NgbDateParserFormatter, NgbDateStruct }   from   '@ng-bootstrap/ng-bootstrap';
 
 @Component({
   selector: 'app-barleft-adm',
   templateUrl: './barleft-adm.component.html',
-  styleUrls: ['./barleft-adm.component.css']
+  styleUrls: ['./barleft-adm.component.css'],
+  providers :  [{ provide: NgbDateParserFormatter ,   useClass :   NgbDateCustomParserFormatter } ]
+  
+  
 })
 export class BarleftAdmComponent implements OnInit {
-  
-  
+
+  model: NgbDateStruct;
   opcion=1;
   constructor() { }
 

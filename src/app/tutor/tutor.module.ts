7418aae4d2ef6adbd85f791e4d83364e15d426ca
LuisLeonardo98 <luis.leonardo.98@hotmail.com>
2019-10-08 05:13:13 -0500

    Agregado Secciones del Tutor

--- src/app/tutor/tutor.module.ts
+++ src/app/tutor/tutor.module.ts
@@ -7,6 +7,7 @@ import { TutorNavBarComponent } from './tutor-nav-bar/tutor-nav-bar.component';
 import { TutorPanelComponent } from './tutor-panel/tutor-panel.component';
 import { TutorVentanaPrincipalComponent } from './tutor-ventana-principal/tutor-ventana-principal.component';
 import { AppRoutingModule } from '../app-routing.module';
+import {NgxPaginationModule} from "ngx-pagination";
 
 @NgModule({
   declarations: [VistaPrincipalComponent, TutorNavBarComponent, TutorPanelComponent, TutorVentanaPrincipalComponent],
@@ -15,10 +16,11 @@ import { AppRoutingModule } from '../app-routing.module';
     FormsModule,
     ReactiveFormsModule,
     NgbModule,
-    AppRoutingModule
+    AppRoutingModule,
+    NgxPaginationModule
   ],
   exports: [
-    VistaPrincipalComponent
+    TutorVentanaPrincipalComponent
   ]
 })
 export class TutorModule { }
--- src/app/tutor/vista-principal/vista-principal.component.ts
+++ src/app/tutor/vista-principal/vista-principal.component.ts
@@ -1,15 +1,63 @@
-import { Component, OnInit } from "@angular/core";
+import { Component, OnInit, Input, PipeTransform } from "@angular/core";
+import { DecimalPipe } from '@angular/common';
+import { FormControl } from '@angular/forms';
+import { Observable } from 'rxjs';
+import { map, startWith } from 'rxjs/operators';
+import { TutorService } from '../../services/tutor.service';
+import { Alumno } from '../../models/alumno';
+import { Seccion } from '../../models/seccion';
 
 @Component({
   selector: "app-vista-principal",
   templateUrl: "./vista-principal.component.html",
-  styleUrls: ["./vista-principal.component.css"]
+  styleUrls: ["./vista-principal.component.css"],
+  providers: [DecimalPipe]
 })
 export class VistaPrincipalComponent implements OnInit {
-  constructor(
+  @Input() numeroTrabajador;
+  pageSize: number = 8;
+  alumnos: Alumno[];
+  alumnos$: Observable<Alumno[]>;
+  secciones: Seccion[];
+  seccionSeleccionada: number;
+  alumnoSeleccionado: string;
+  filter = new FormControl("");
+
+  constructor( private pipe: DecimalPipe, private tutorService: TutorService
   ) {}
 
   ngOnInit() {
+    this.tutorService.getSeccionesFromTrabajador(this.numeroTrabajador).subscribe(data => {
+      this.secciones = data;
+      if(this.secciones.length > 0) {
+        this.seccionSeleccionada = this.secciones[0].id;
+        this.tutorService.getAlumnosFromSeccion(this.secciones[0].id).subscribe(data => {
+          this.alumnos = data;
+          this.alumnos$ = this.filter.valueChanges.pipe(
+            startWith(""),
+            map(text => this.search(text, this.pipe))
+          );
+          console.log(this.alumnos);
+        });
+      }
+    })
   }
 
+  cambiarSeccion() {
+    this.tutorService.getAlumnosFromSeccion(this.seccionSeleccionada).subscribe(data => {
+      this.alumnos = data;
+      this.alumnos$ = this.filter.valueChanges.pipe(
+        startWith(""),
+        map(text => this.search(text, this.pipe))
+      );
+      console.log(this.alumnos);
+    });
+  }
+
+  search(text: string, pipe: PipeTransform): Alumno[] {
+    return this.alumnos.filter(alumno => {
+      const term = text.toLowerCase();
+      return alumno.nombre.toLowerCase().includes(term);
+    });
+  }
 }

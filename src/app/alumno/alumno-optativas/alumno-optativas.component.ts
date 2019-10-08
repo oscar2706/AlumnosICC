a1de198508bd7d34bbf187a25474a5f615779579
oscar2706 <spat_2706@outlook.com>
2019-10-08 10:17:01 -0500

    Agregado Mapa Optativas

--- src/app/alumno/alumno-optativas/alumno-optativas.component.ts
+++ src/app/alumno/alumno-optativas/alumno-optativas.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Materia } from 'src/app/models/materia';
+import { AlumnoService } from 'src/app/services/alumno.service';
 
 @Component({
   selector: 'app-alumno-optativas',
@@ -6,10 +8,31 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./alumno-optativas.component.css']
 })
 export class AlumnoOptativasComponent implements OnInit {
+subscription;
 
-  constructor() { }
+  materias: Materia[] = [];
+  constructor(private alumnoService: AlumnoService) { }
 
-  ngOnInit() {
+  ngOnInit () {
+    this.subscription = this.alumnoService.getMapa().subscribe(data => {
+      this.materias = data;
+    });
+  }
+
+  ngOnDestroy () {
+    this.subscription.unsubscribe();
+  }
+
+  getMateria (clave: string): Materia {
+    let materia_encontrada: Materia;
+    this.materias.forEach(materia => {
+      if (materia.clave == clave) {
+        // console.log('materia encontrada');
+        // console.log(materia);
+        materia_encontrada = materia;
+      }
+    });
+    return materia_encontrada;
   }
 
 }
